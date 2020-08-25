450bc12d499100562c26c9f4510da17f747af895
Linxuan-Chen <moshouclxclx@163.com>
2020-08-25 14:30:15 -0400

    Updates

---
+++ precache-manifest.0c0be315f1e8db03a8dfdac86ff5e1d7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "61046aef08f0488579faa9f1fa960d50",
+    "url": "/index.html"
+  },
+  {
+    "revision": "16d712e843ae770daeab",
+    "url": "/static/css/main.a63ce57e.chunk.css"
+  },
+  {
+    "revision": "c28e8076693cbecb2d34",
+    "url": "/static/js/2.00a02c7d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.00a02c7d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "16d712e843ae770daeab",
+    "url": "/static/js/main.b9db6962.chunk.js"
+  },
+  {
+    "revision": "711714d7d018d95c9ba4",
+    "url": "/static/js/runtime-main.33471999.js"
+  }
+]);

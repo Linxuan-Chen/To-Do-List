d26b355e4a4dbe81a9f7cf5aaf68c41822aa8bd9
Linxuan-Chen <moshouclxclx@163.com>
2020-11-09 12:21:42 -0500

    Improving User experience

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,24 +1,37 @@
+/*
+ * @Author: your name
+ * @Date: 2020-08-24 13:17:36
+ * @LastEditTime: 2020-11-09 12:20:41
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: \To do list\To-Do-List\src\components\Header\index.js
+ */
 import React from "react";
 
 import "./index.scss";
 
 export default function Header(props) {
-
   const { setIsInputShow, isInputShow } = props;
 
   /**
    * @description: Change isInputShow flag to opposite when plus button is triggered
-   */  
+   */
   const plusButtonClickHandler = () => {
-    setIsInputShow(!isInputShow)
+    setIsInputShow(!isInputShow);
   };
 
   return (
     <header className="header container">
       <div className="row bg-primary">
-        <div className="col-10 text-center text-white p-0">To Do list</div>
+        <div className="col-10 text-center text-white p-0">
+          Please Press Plus Button To Add A New Task
+        </div>
         <div className="col-2 p-0 text-right pr-3">
-          <div className="btn pt-0 text-white text-center" onClick={plusButtonClickHandler}>
+          <div className="col-12"></div>
+          <div
+            className="btn pt-0 text-white text-center"
+            onClick={plusButtonClickHandler}
+          >
             &#43;
           </div>
         </div>
